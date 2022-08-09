f08a8f0e693440ba6b5255a3b0f3b7b9e9dc1f81
aLEGEND21 <arnavmurthi@gmail.com>
2022-08-09 11:50:36 -0400

    Attempt to fix setup.py not working

---
+++ _setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# Requirements
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="async-yfinance-api",
+    version="0.1.0",
+    description="A Python module to fetch financial data from Yahoo Finance.",
+    author="aLEGEND21",
+    packages=find_packages(),
+    install_requires=requirements,
+)
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
-from setuptools import find_packages
-
-# Requirements
-requirements = []
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+import setuptools
 
 setup(
-    name="async-yfinance-api",
-    version="0.1.0",
-    description="A Python module to fetch financial data from Yahoo Finance.",
-    author="aLEGEND21",
-    packages=find_packages(),
-    install_requires=requirements,
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='async-yfinance-api',
+    url='https://github.com/aLEGEND21/async-yfinace-api',
+    author='aLEGEND21',
+    author_email='_',
+    # Needed to actually package something
+    packages=setuptools.find_packages(),
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version='0.1.0',
+    # The license can be anything you like
+    license='MIT',
+    description='Get more classes that numbers can be sorted into.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
 )
