2ff0b4a739dcedbc863f847b8c95dec5439c05a1
aLEGEND21 <arnavmurthi@gmail.com>
2022-08-09 11:53:21 -0400

    Attempt fix

--- yfinance/__init__.py
+++ yfinance/__init__.py
@@ -1,6 +1,6 @@
-"""A Python module to fetch financial data from Yahoo Finance."""
-
-__all__ = ["__version__", "fetch_summary"]
-__version__ = '0.1.0'
-
 from .api import *
+
+__all__ = ["name", "__version__", "fetch_summary"]
+
+name = "async-yfinance-api"
+__version__ = "0.1.0"
